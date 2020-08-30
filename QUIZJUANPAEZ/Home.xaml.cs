03d2e3257c098c34c98e60db83b23947a4786f89
Juan Esteban Paez Albarracin <juan.paez.2020@upb.edu.co>
2020-08-30 17:04:38 -0500

    insertar

--- QUIZJUANPAEZ/Home.xaml.cs
+++ QUIZJUANPAEZ/Home.xaml.cs
@@ -21,6 +21,41 @@ namespace QUIZJUANPAEZ
         public Home()
         {
             InitializeComponent();
+            cmbItms.Items.Add("Rings");
+            cmbItms.Items.Add("Earrings");
+            cmbItms.Items.Add("Necklace");
+            cmbItms.Items.Add("Belt");
+            cmbItms.Items.Add("Bracelet");
+
         }
+
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = txtProductName.Text;
+            string category = lblCategory.Content.ToString();
+
+            lstdata.Items.Add(txtProductName.Text);
+            lstdata.Items.Add(lblCategory.Content);
+            lstdata.Items.Add(cmbItms.Text);
+
+        }
+
+        private void btnLogout_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow ss = (MainWindow)Window.GetWindow(this);
+            ss.frameMain.NavigationService.Navigate(new Login());
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if(rb.IsChecked.Value)
+            {
+                lblCategory.Content = rb.Content.ToString();
+            }
+        }
+
     }
 }
