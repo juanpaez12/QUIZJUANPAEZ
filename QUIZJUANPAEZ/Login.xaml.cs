7836deecc82d0f9f60a53a00f89ea4417de7de45
Juan Esteban Paez Albarracin <juan.paez.2020@upb.edu.co>
2020-08-29 22:56:32 -0500

    insertar

---
+++ QUIZJUANPAEZ/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QUIZJUANPAEZ
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "Juan" && txtPassword.Password == "1234")
+            {
+                MainWindow w = (MainWindow)Window.GetWindow(this);
+                w.frameMain.NavigationService.Navigate(new Home());
+            }
+            else
+            {
+                MessageBox.Show("Invalid Credentials");
+            }
+        }
+    }
+}
---
+++ QUIZJUANPAEZ/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace QUIZJUANPAEZ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            frameMain.NavigationService.Navigate(new Login());
+        }
+
+    }
+}
